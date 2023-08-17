4c3eff608f10bac7ae532ceb59d30a773f6b563e
Artem Khrapuzov <tema102@mail.ru>
2023-08-17 16:30:47 +0600

    create navbar

--- advice/urls.py
+++ advice/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main_menu),
+    path('', views.main_menu, name='main_menu'),
     path('workout/', views.workout, name='workout'),
     path('nutrition/', views.nutrition, name='nutrition'),
     path('disease/', views.disease, name='disease')
