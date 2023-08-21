4d4c4e3af0e19c170b6b416f0b7f55bb80ac4ce9
Artem Khrapuzov <tema102@mail.ru>
2023-08-21 11:08:56 +0600

    create light css and add image in training/

--- advice/models.py
+++ advice/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 class Muscle(models.Model):
     name = models.CharField(max_length=50)
     function = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='image/')
 
     def __str__(self):
         return self.name
--- sport/urls.py
+++ sport/urls.py
@@ -1,7 +1,14 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from sport import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('advice.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
